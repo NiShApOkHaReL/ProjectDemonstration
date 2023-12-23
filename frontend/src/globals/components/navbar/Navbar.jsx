166ef9514065f66275b7044fadf117805153cdcf
Nisha Pokharel <nishapokharel2018@gmail.com>
2023-12-23 16:04:22 +0545

    updated

--- frontend/src/globals/components/navbar/Navbar.jsx
+++ frontend/src/globals/components/navbar/Navbar.jsx
@@ -1,10 +1,31 @@
+
 import React from 'react';
 
-const NavBar = () => {
+const Navbar = () => {
   return (
-    <>Navbar</>
-    
+    <nav className="bg-purple-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold text-xl">Smart Municipality</div>
+        <div className="space-x-4">
+          <NavLink to="/" label="Home" />
+          <NavLink to="/report-problem" label="Report Problem" />
+          <NavLink to="/view-report" label="View Your Report" />
+          <NavLink to="/help" label="Help" />
+          <button className="text-white" onClick={() => console.log('Logout clicked')}>
+            Logout
+          </button>
+        </div>
+      </div>
+    </nav>
   );
 };
 
-export default NavBar;
+const NavLink = ({ to, label }) => {
+  return (
+    <a href={to} className="text-white hover:underline">
+      {label}
+    </a>
+  );
+};
+
+export default Navbar;
