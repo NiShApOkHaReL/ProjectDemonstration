db9c11c7d04c8a2c41266be8ea7997542298efaa
Nisha Pokharel <nishapokharel2018@gmail.com>
2023-12-23 17:48:46 +0545

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 
 import './App.css'
-import NavBar from './globals/components/navbar/Navbar'
-import Login from './globals/auth/login/Login'
+import Navbar from './globals/components/navbar/Navbar'
+import Footer from './globals/components/footer/Footer'
+import Register from './pages/auth/register/Register'
+import Login from './pages/auth/login/Login'
+import Home from './pages/home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-    <Login/>
-    <NavBar/>
-    </>
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    <Footer />
+  </>
   )
 }
 
