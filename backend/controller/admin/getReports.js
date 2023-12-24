f887e06b0b7b7f38bfb5d509e881fe775e4b0692
Garima <paudelgarima229@gmail.com>
2023-12-24 13:40:24 +0545

    some more changes

--- backend/controller/admin/getReports.js
+++ backend/controller/admin/getReports.js
@@ -1,4 +1,5 @@
 const Reports = require("../../model/reportsModel")
+const User = require("../../model/userModel")
 
 exports.getReports= async(req,res)=>{
     const reports = await Reports.find()
@@ -13,3 +14,34 @@ exports.getReports= async(req,res)=>{
         data : reports
     })
 }
+
+exports.getSingleOrder = async(req,res)=>{
+    const {id} = req.params 
+    // check if order exist or not 
+    const report= await Reports.findById(id)
+    if(!report){
+        return res.status(404).json({
+            message : "No report found with that id"
+        })
+    }
+    res.status(200).json({
+        message : "Report fetched successfully",
+        data : report
+    })
+}
+
+exports.viewUsers = async(req,res)=>{
+    const users = await User.find()
+    if(users.length == 0){
+       return res.status(404).json({        
+        message : "Users not found",
+        data : []
+       })
+    }
+    res.status(200).json({
+        message : "Users fetched successfully",
+        data : users
+    })
+}
+
+
--- backend/routes/admin/adminRoutes.js
+++ backend/routes/admin/adminRoutes.js
@@ -1,9 +1,10 @@
-const { getReports } = require("../../controller/admin/getReports")
+const { getReports, viewUsers } = require("../../controller/admin/getReports")
 const isAuthenticated = require("../../middleware/isAuthenticated")
 const restrictTo = require("../../middleware/restrictTo")
 
 const router = require("express").Router()
 
 router.route("/getReports").get(isAuthenticated,restrictTo('admin'), getReports )
+router.route("/users").get(isAuthenticated,restrictTo('admin'),viewUsers)
 
 module.exports = router
