88ddf7b8a734a12df0ad8a98efc7945231982993
Nisha Pokharel <nishapokharel2018@gmail.com>
2023-12-24 20:55:30 +0545

    Frontend

--- frontend/src/globals/components/footer/Footer.jsx
+++ frontend/src/globals/components/footer/Footer.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-purple-600 text-white p-4">
-      <div className="container mx-auto text-center">
-        <p>&copy; 2023 Your App. All rights reserved.</p>
+    <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap md:text-left text-center order-first">
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+            <nav className="list-none mb-10">
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              </li>
+            </nav>
+          </div>
+          {/* Repeat the above structure for other sections */}
+        </div>
+      </div>
+      <div className="bg-gray-100">
+        <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
+            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <a className="text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            {/* Add more social icons as needed */}
+          </span>
+        </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
--- frontend/src/pages/user/Report.jsx
+++ frontend/src/pages/user/Report.jsx
@@ -1,29 +1,142 @@
-import React from 'react';
+// Report.js
+import React, { useState } from 'react';
 import { Route, Routes, Link, Outlet } from 'react-router-dom';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './Report.css'; // Import your CSS file for styling
+
+const ReportPart1 = () => {
+  return (
+    <div className="max-w-lg mx-auto p-4 border border-gray-300 rounded">
+      <h2 className="text-2xl mb-4">Report Part 1</h2>
+      <div className="mb-4">
+        <label htmlFor="problemcat" className="block text-xl mb-2">
+          Choose the category
+        </label>
+        <select
+          name="problemcat"
+          id="problemcat"
+          className="h-14 w-80 rounded-md m-3 border-gray-400 border-2"
+        >
+          <option value="education">Education</option>
+          <option value="infrastructures">Infrastructures</option>
+          <option value="electricity">Electricity</option>
+          <option value="agriculture">Agriculture </option>
+          <option value="land use">Land Use</option>
+          <option value="health">Health</option>
+          <option value="water supply">Water Supply</option>
+          <option value="drainage">Drainage</option>
+          <option value="culture and religion">Culture and Religion</option>
+          <option value="Security">Security</option>
+          <option value="others">Others</option>
+        </select>
+      </div>
+      <div className="flex justify-center items-center">
+        <textarea
+          id="problem"
+          name="problem"
+          placeholder="Explain your problem here"
+          className="h-20 w-11/12  rounded-md m-3 border-gray-400 border-2"
+        ></textarea>
+      </div>
+      <div>
+        <label className="text-xl m-3" htmlFor="imageFile">
+          Insert Image
+        </label>{' '}
+        <br />
+        <input
+          type="file"
+          name="imageFile"
+          id="imageFile"
+          className="text-xl m-3"
+        />
+      </div>
+      <div>
+        <p className="text-xl m-3">How much emergency is your problem?</p>
+        <label htmlFor="emer" className="text-xl m-3">
+          Emergency Status:
+        </label>
+        <select
+          name="emer"
+          id="emer"
+          className="h-14 w-80 rounded-md m-3 border-gray-400 border-2"
+        >
+          <option value="notemer">Not emergency</option>
+          <option value="emergency">High Emergency</option>
+        </select>
+      </div>
+      <Link to="part2" className="bg-blue-500 text-white px-4 py-2 rounded">
+        Next
+      </Link>
+    </div>
+  );
+};
+
+const ReportPart2 = () => {
+  const mapRef = React.useRef(null);
+  const [pinCoordinates, setPinCoordinates] = useState({ lat: 0, lng: 0 });
+
+  const handleMapClick = (e) => {
+    const { lat, lng } = e.latlng;
+    setPinCoordinates({ lat, lng });
+  };
+
+  React.useEffect(() => {
+    // Initialize the map only if it doesn't exist
+    if (!mapRef.current) {
+      const mapInstance = L.map('map').setView([0, 0], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors',
+      }).addTo(mapInstance);
+
+      mapRef.current = mapInstance;
+    }
+
+    const map = mapRef.current;
+
+    // Cleanup function to remove the map when the component unmounts
+    return () => {
+      if (map) {
+        map.remove();
+      }
+    };
+  }, []);
+
+  return (
+    <div className="max-w-lg mx-auto p-4 border border-gray-300 rounded">
+      <h2 className="text-2xl mb-4">Report Part 2</h2>
+      <div className="mb-4">
+        <p className="text-xl mb-2">Pinpoint your location on the map:</p>
+        <div
+          id="map"
+          className="h-64 w-full rounded-md border-gray-400 border-2 mb-4"
+          onClick={handleMapClick}
+        ></div>
+        {pinCoordinates.lat !== 0 && pinCoordinates.lng !== 0 && (
+          <p className="text-xl mb-2">
+            Latitude: {pinCoordinates.lat}, Longitude: {pinCoordinates.lng}
+          </p>
+        )}
+      </div>
+      <Link to="/Report" className="bg-blue-500 text-white px-4 py-2 rounded">
+        Back to Part 1
+      </Link>
+    </div>
+  );
+};
+
+
+
+
 
 const Report = () => {
   return (
     <div>
-      <h1>Report Page</h1>
+      <h1 className="text-3xl font-bold mb-4">Report Page</h1>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <h2>Report Part 1</h2>
-              <Link to="part2">Next</Link>
-            </div>
-          }
-        />
-        <Route
-          path="part2"
-          element={
-            <div>
-              <h2>Report Part 2</h2>
-              {/* Your code for the second part */}
-            </div>
-          }
-        />
+        <Route path="/" element={<ReportPart1 />} />
+        <Route path="/part2" element={<ReportPart2 />} />
       </Routes>
       <Outlet />
     </div>
