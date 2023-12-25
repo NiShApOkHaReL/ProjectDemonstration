40ed45e4a08b89b08184d377ebcc51926fd73c8f
Nisha Pokharel <nishapokharel2018@gmail.com>
2023-12-25 08:20:09 +0545

    footer added

--- frontend/src/globals/components/footer/Footer.jsx
+++ frontend/src/globals/components/footer/Footer.jsx
@@ -2,51 +2,59 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap md:text-left text-center order-first">
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-            <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-              </li>
-            </nav>
-          </div>
-          {/* Repeat the above structure for other sections */}
+    <div className="bg-purple-800 text-white">
+      <div className="max-w-screen-lg py-10 px-4 sm:px-6 sm:flex justify-between mx-auto">
+        <div className="p-5 sm:w-2/12 border-r">
+          <div className="text-sm uppercase text-indigo-300 font-bold">Menu</div>
+          <ul>
+            <li className="my-2">
+              <a className="hover:text-indigo-300" href="#">
+                Home
+              </a>
+            </li>
+            <li className="my-2">
+              <a className="hover:text-indigo-300" href="#">
+                Services
+              </a>
+            </li>
+            <li className="my-2">
+              <a className="hover:text-indigo-300" href="#">
+                Products
+              </a>
+            </li>
+            <li className="my-2">
+              <a className="hover:text-indigo-300" href="#">
+                Pricing
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="bg-gray-100">
-        <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">Tailblocks</span>
-          </a>
-          <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
-            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+        <div className="p-5 sm:w-7/12 border-r text-center">
+          <h3 className="font-bold text-xl text-indigo-300 mb-4">Componentity</h3>
+          <p className="text-indigo-300 text-sm mb-10">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s.
           </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a className="text-gray-500">
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            {/* Add more social icons as needed */}
-          </span>
+        </div>
+        <div className="p-5 sm:w-3/12">
+          <div className="text-sm uppercase text-indigo-300 font-bold">Contact Us</div>
+          <ul>
+            <li className="my-2">
+              <a className="hover:text-indigo-300" href="#">
+                XXX XXXX, Floor 4 San Francisco, CA
+              </a>
+            </li>
+            <li className="my-2">
+              <a className="hover:text-indigo-300" href="#">
+                contact@company.com
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    </footer>
+
+      <div className="my-5 text-center">© Copyright 2023. All Rights Reserved.</div>
+    </div>
   );
 };
 
